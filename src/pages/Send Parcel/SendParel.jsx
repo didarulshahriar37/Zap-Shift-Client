7e9ea23d9ee09a62f1e2cac423dce5a9ec822cbb
Didarul Shahriar <didarul13579@gmail.com>
2025-11-22 13:06:37 +0600

    added payment section

--- src/pages/Send Parcel/SendParel.jsx
+++ src/pages/Send Parcel/SendParel.jsx
@@ -51,6 +51,8 @@ const SendParel = () => {
         }
 
         console.log(cost);
+        data.cost = cost;
+
         Swal.fire({
             title: "Agree with the cost?",
             text: `You will be charged ${cost}`,
